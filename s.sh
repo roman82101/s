370d486a81ef56322ebe60bc760ae40dde698318
roman82101 <47818052+roman82101@users.noreply.github.com>
2022-04-03 18:16:37 +0300

    Update s.sh

--- s.sh
+++ s.sh
@@ -54,7 +54,6 @@ rm -f ufw.sh ufw-m.sh
 
 
 echo "... Установка сервера Shadowsocks ..."
-#!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #
