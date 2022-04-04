99b3443dfc5226024928f1c992ff76229250756d
roman82101 <47818052+roman82101@users.noreply.github.com>
2022-04-04 20:11:42 +0300

    Update s.sh

--- s.sh
+++ s.sh
@@ -1152,35 +1152,6 @@ verb 3" >>/etc/openvpn/client-template.txt
 	newClient
 	echo "If you want to add more clients, you simply need to run this script another time!"
 
-function manageMenu() {
-	echo "Welcome to OpenVPN-install!"
-	echo "The git repository is available at: https://github.com/angristan/openvpn-install"
-	echo ""
-	echo "It looks like OpenVPN is already installed."
-	echo ""
-	echo "What do you want to do?"
-	echo "   1) Add a new user"
-	echo "   2) Revoke existing user"
-	echo "   3) Remove OpenVPN"
-	echo "   4) Exit"
-	until [[ $MENU_OPTION =~ ^[1-4]$ ]]; do
-		read -rp "Select an option [1-4]: " MENU_OPTION
-	done
-	case $MENU_OPTION in
-	1)
-		newClient
-		;;
-	2)
-		revokeClient
-		;;
-	3)
-		removeOpenVPN
-		;;
-	4)
-		exit 0
-		;;
-	esac
-}
 # Check for root, TUN, OS...
 initialCheck
 # Check if OpenVPN is already installed
